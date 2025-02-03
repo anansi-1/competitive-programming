9dcb83a2cce21becde9ec794d615182376d7e049
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-03 21:55:37 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237435.py
@@ -0,0 +1,9 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n = int(input())
+for _ in range(n):
+    word = input()
+    if len(word) > 10:
+        print(word[0] + f"{len(word)-2}" +word[-1])
+    else:
+        print(word)
