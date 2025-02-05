6971cc09a1bc1ceb38b9f8d0daad5f9329352256
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-05 05:29:42 +0300

    Created 'Pass the Pillow' by A2SV Hub

---
+++ 05-Feb-2025/Pass the Pillow 241844.py
@@ -0,0 +1,11 @@
+# Problem: Pass the Pillow - https://leetcode.com/problems/pass-the-pillow/description/
+
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:    
+        rounds = time// (n-1)
+        ans = 0
+        if (rounds%2 == 0):
+            ans = (1+time%(n-1))
+        else:
+            ans = (n-time%(n-1))
+        return ans
