e715fe8ed4902af345d218de688206c014a957f7
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-23 20:48:17 +0300

    Created 'Partition List' by A2SV Hub

---
+++ 23-Feb-2025/Partition List 281194.py
@@ -0,0 +1,24 @@
+# Problem: Partition List - https://leetcode.com/problems/partition-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        less_head = ListNode()
+        greater_head = ListNode()  
+        less = less_head
+        greater = greater_head 
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next  
+        greater.next = None
+        less.next = greater_head.next
+        return less_head.next
