6b719f5b2e17aacae82e62a196abd4514da96b25
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-25 22:33:11 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 25-Feb-2025/Alternating Subsequence 282771.py
@@ -0,0 +1,16 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+testcases=int(input())
+for _ in range(testcases):
+    n=int(input())
+    nums=list(map(int,input().split()))  
+    i=0
+    max_sum=0
+    while i<len(nums):
+        max_value=nums[i]
+        while i+1<len(nums) and nums[i]*nums[i+1]>0:
+            max_value=max(max_value,nums[i+1])
+            i+=1
+        max_sum+=max_value
+        i+=1   
+    print(max_sum)
