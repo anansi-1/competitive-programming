ce27788886b47f9cd104892952f012653eeaa3aa
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-05 14:29:10 +0300

    Created 'Maximum Odd Binary Number' by A2SV Hub

---
+++ 05-Mar-2025/Maximum Odd Binary Number 292801.py
@@ -0,0 +1,10 @@
+# Problem: Maximum Odd Binary Number - https://leetcode.com/problems/maximum-odd-binary-number/
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        s = sorted(s,reverse=True)
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == "1":
+                s[i],s[-1] = s[-1],s[i]
+                break
+        return "".join(s)
