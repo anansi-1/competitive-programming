5d79a8bae6048235caa9ae12406838cf75f5be3c
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-11 15:35:48 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 11-Mar-2025/D - Repeating Cipher 302421.py
@@ -0,0 +1,13 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+s = input()
+curr = 0
+ans = []
+ans.append(s[0])
+for i in range(1,len(s)):
+    curr += i    
+    if curr >= len(s):
+        break
+    ans.append(s[curr])
+print("".join(ans))
