3a4e1fcb0b654ac2f5c4f7bcd6b05f6f257a2c1a
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-11 15:04:36 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 11-Mar-2025/C - Compare T-Shirt Sizes 302365.py
@@ -0,0 +1,21 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+t = int(input())
+value = {"S": 0, "M": 1, "L": 2}
+for _ in range(t):
+    a, b = input().split()
+    if a == b:
+        print("=")
+    else:
+        if a[-1] == b[-1]:  
+            c_a = a.count("X")
+            c_b = b.count("X")
+            if c_a > c_b:
+                print(">" if a[-1] == "L" else "<")
+            else:
+                print("<" if a[-1] == "L" else ">")
+        else:
+            if value[a[-1]] > value[b[-1]]:
+                print(">")
+            else:
+                print("<")
