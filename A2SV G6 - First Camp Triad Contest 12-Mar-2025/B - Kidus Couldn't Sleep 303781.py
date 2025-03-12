63c2cb446f568e5aa36df341fc1c79dd2b2a5e46
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-12 14:33:08 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 12-Mar-2025/B - Kidus Couldn't Sleep 303781.py
@@ -0,0 +1,10 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n,k = map(int,input().split())
+arr = list(map(int,input().split()))
+curr = sum(arr[:k])
+t = curr
+for i in range(k,n):
+    curr += arr[i] - arr[i-k]
+    t += curr
+print(t/ (n-k+1))
