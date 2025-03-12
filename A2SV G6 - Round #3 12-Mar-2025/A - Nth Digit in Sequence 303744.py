df5ba19a8aac056472b7590a9a074271acd6b8f1
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-12 13:46:08 +0300

    Created 'A - Nth Digit in Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #3 12-Mar-2025/A - Nth Digit in Sequence 303744.py
@@ -0,0 +1,8 @@
+# Problem: A - Nth Digit in Sequence - https://codeforces.com/gym/588468/problem/A
+
+num = int(input())
+ans = []
+for i in range(1,num+1):
+    ans.append(i)
+strr = "".join(map(str,ans))
+print(strr[num-1])
