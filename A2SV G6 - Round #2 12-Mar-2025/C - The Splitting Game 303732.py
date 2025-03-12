31d8fe1c1e7a7d70238ac91054e6277be0226f78
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-12 13:31:50 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 12-Mar-2025/C - The Splitting Game 303732.py
@@ -0,0 +1,18 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    r = Counter(s)
+    l = Counter()
+    maxx_sum = 0      
+    for i in s:
+        l[i] += 1
+        r[i] -= 1
+        if r[i] == 0:
+            del r[i]
+        curr_sum = len(l) + len(r)
+        maxx_sum = max(maxx_sum, curr_sum)
+    print(maxx_sum)
