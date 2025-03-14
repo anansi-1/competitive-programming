4ce891ee99d5acd69854d4c9d3b60f5a15273376
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-14 19:23:09 +0300

    Created 'Binary Tree Inorder Traversal' by A2SV Hub

---
+++ 14-Mar-2025/Binary Tree Inorder Traversal 306989.py
@@ -0,0 +1,20 @@
+# Problem: Binary Tree Inorder Traversal - https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def helper(node):
+            if node == None:
+                return
+            helper(node.left)
+            res.append(node.val)
+            helper(node.right)
+        helper(root)
+        return res
+        
