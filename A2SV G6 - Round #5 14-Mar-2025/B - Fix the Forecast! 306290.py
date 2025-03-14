25bff568f7aeca8458ebaeaee0ffb8b9d488d1a9
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-14 11:37:26 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 14-Mar-2025/B - Fix the Forecast! 306290.py
@@ -0,0 +1,19 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    b = list(map(int,input().split()))
+    b.sort() 
+    a_index = []
+    ans = [0]*len(a)
+    for i in range(len(a)):
+        a_index.append((a[i],i))
+    a_index.sort()
+    for i in range(len(b)):
+        idx = a_index[i][1]
+        ans[idx] = b[i]
+    for i in ans:
+        print(i,end=" ")
+    print()
