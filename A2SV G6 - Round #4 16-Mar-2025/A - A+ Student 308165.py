0a7c84098f74bdafa968c0140078119156709966
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-16 10:15:00 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 16-Mar-2025/A - A+ Student 308165.py
@@ -0,0 +1,9 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+testcases = int(input())
+for _ in range((testcases)):
+    a,b,c = map(int,input().split())
+    f = max(0,max(b,c)-a+1)
+    s =max(0,max(a,c)-b + 1)
+    t = max(0,max(a,b)-c + 1)
+    print(f,s,t)
