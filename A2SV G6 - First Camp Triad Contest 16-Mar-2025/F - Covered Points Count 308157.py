be6a29b886fec4e9d43bab83b4711212fb6e1bfb
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-16 09:49:52 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 16-Mar-2025/F - Covered Points Count 308157.py
@@ -0,0 +1,21 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+from collections import defaultdict
+t = int(input())
+points = [tuple(map(int,input().split())) for _ in range(t)]
+
+coordinate = defaultdict(int)
+
+for l,r in points:
+    coordinate[l] += 1
+    coordinate[r+1] -= 1
+keys = sorted(coordinate.keys())
+coverage = 0
+prev = keys[0]
+result = defaultdict(int)
+for point in keys:
+    result[coverage] += point - prev
+    coverage += coordinate[point]
+    prev = point
+for k in range(1,t+1):
+    print(result[k],end=" ")
