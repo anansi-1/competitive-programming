2d36d8847c9378de139d1854eb151209dcfe008b
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-16 10:04:31 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 16-Mar-2025/D - Minimize the Distance 308160.py
@@ -0,0 +1,9 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+num = list(map(int,input().split()))
+num.sort()
+if n%2 == 1:
+     print(num[n//2])
+else:
+     print(num[(n//2)-1])
