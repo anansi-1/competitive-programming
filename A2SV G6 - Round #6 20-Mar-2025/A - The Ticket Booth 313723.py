f6ae55c470056ecf623dbcb6d426dfb3c4c3192e
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-20 22:40:03 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 20-Mar-2025/A - The Ticket Booth 313723.py
@@ -0,0 +1,9 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+n,s = map(int,input().split())
+count = 0
+if n >= s:
+    print(1)
+else:
+    count = (s+n -1)//n
+    print(count)
