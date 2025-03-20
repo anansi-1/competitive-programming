88c28f4abc914594efaa4ef0c711641ad3639d25
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-20 22:50:51 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 20-Mar-2025/B - Nafargi's Binary Reduction Game 313746.py
@@ -0,0 +1,12 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+lenn = int(input())
+n = input()
+s = []
+s.append(n[0])
+for i in range(1,len(n)):
+    if s and s[-1] != n[i]:
+        s.pop()
+    else:
+        s.append(n[i])
+print(len(s))
