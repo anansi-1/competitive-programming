fb40bdd05c631b32698986920387a82b99e11c47
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-10-21 18:03:51 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 21-Oct-2025/Red and Blue 404891.py
@@ -0,0 +1,18 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    r = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input(). split()))
+    r.append(0)
+    b.append(0)
+    maxr,maxb = 0,0 
+    for i in range(1, len(r)):
+        r[i] += r[i-1]
+        maxr=max(maxr, r[i-1])
+    for i in range(1, len(b)):
+        b[i] += b[i-1]
+        maxb=max(maxb, b[i-1])
+    print(maxb+maxr)
