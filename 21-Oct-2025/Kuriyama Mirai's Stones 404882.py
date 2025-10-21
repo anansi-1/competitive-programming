080e3c68319853c96ead93804232d3e42a001917
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-10-21 17:37:15 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 21-Oct-2025/Kuriyama Mirai's Stones 404882.py
@@ -0,0 +1,17 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+a = [int(i) for i in input().split()]
+a1 = sorted(a)
+b1  = [0] * (n + 1)
+b2 = [0] * (n + 1)
+for i in range(n):
+    b1[i + 1] = b1[i] + a[i]
+    b2[i + 1] = b2[i] + a1[i]
+m = int(input())
+for i in range(m):
+    t, l, r = [int(i) for i in input().split()]
+    if t == 1:
+        print(b1[r] - b1[l - 1])
+    else:
+        print(b2[r] - b2[l - 1])
