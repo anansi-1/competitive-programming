270816169c14ed01211bb6ce6c1ed6434b316fbc
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-10-23 18:34:02 +0300

    Created 'Minimum Integer' by A2SV Hub

---
+++ 23-Oct-2025/Minimum Integer 405307.py
@@ -0,0 +1,11 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+t = int(input())
+for _ in range(t):
+	l,r,d = list(map(int, input().split()))
+	if d < l:
+		print(d)
+	elif d > r:
+		print(d)
+	else:
+		print(((r)//d+1)*d)
