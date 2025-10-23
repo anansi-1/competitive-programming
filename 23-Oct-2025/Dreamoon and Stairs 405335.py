e95e63a745a8389a0c2eb12ddbad39d196448645
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-10-23 19:26:43 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 23-Oct-2025/Dreamoon and Stairs 405335.py
@@ -0,0 +1,11 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+n, m = map(int, input().split())
+i = 0
+if n < m :
+    print(-1)
+else :
+    while n > 0 :
+        n -= 2*m
+        i += 1
+    print(m*i)
